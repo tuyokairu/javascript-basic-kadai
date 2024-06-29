93823dc9b3e55fb49b230ee1535bb80f22ee8067
tuyokairu <yuri492@yahoo.co.jp>
2024-06-29 14:04:03 +0900

    kdai_020

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,10 +1,6 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
-const text2 = document.getElementById('text2');
 
 btn.addEventListener('click',() => {
-  const childList = document.createElement('h4');
-  childList.textContent = 'ボタンをクリックしました';
-  text2.appendChild(childList);
-  text.style.display= 'none';
+  text.textContent = 'ボタンをクリックしました';
 })
